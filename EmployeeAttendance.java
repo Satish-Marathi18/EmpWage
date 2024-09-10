d1197d5b6d3723211f65552bcf55eec61df1f355
Satish-Marathi18 <satishmarathi17@gmail.com>
2024-09-10 21:57:11 +0530

    first commit

---
+++ EmployeeAttendance.java
@@ -0,0 +1,16 @@
+public class EmployeeAttendance {
+    public static void main(String[] args) {
+        System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if(empCheck == 0)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
+
+    }
+}
